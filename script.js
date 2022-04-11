9c4d2312a2d97a1e946eb9218601536a9808c127
Kreevoi <92435504+Kreevoi@users.noreply.github.com>
2022-04-11 17:48:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,46 +7,47 @@ const theme = document.querySelector('#css-link');
   } else {
     theme.href = 'light-thame.css';
   }
-};
+    }
+
 //регистрация на сайте
 function registration(){
     //Ввод ника
-    let playerName = prompt('Придумайте себе имя','Player_1');
+    const playerName = prompt('Придумайте себе имя','Player_1');
     let namePlace = document.querySelector('.player-name');
     namePlace.textContent = playerName;
   
     //Случайное имя противника
-    let computerNameBase = ['Биба', 'Боба', 'Лёлик', 'Болик', 'Жока', 'Бока','Носок судьбы','Тащю Как Путин','ОпАSнЫй ВоЗрАSт', 'Мефедрон','Я ЗгРЫС ДЕДА ','I LoVE BalTikA', 'Пуssи Прокурор', 'СоЛян@я дев04кА'];
+    let computerNameBase = ['Биба', 'Боба', 'Лёлик', 'Болик', 'Жока', 'Бока','anal penetrator','Тащю Как Путин','ОпАSнЫй ВоЗрАSт', 'Мефедрон','Я ЗгРЫС ДЕДА ','I LoVE BalTikA', 'Пуssи Прокурор', 'СоЛян@я дев04кА'];
     let computerNameNum = Math.floor(Math.random()*computerNameBase.length);
-    let computerName = computerNameBase[computerNameNum];
+    const computerName = computerNameBase[computerNameNum];
     let computerNamePlace = document.querySelector('.computer-name');
     computerNamePlace.textContent = computerName;
-  } 
-  
+    } 
+
+//сессия
    function game(){
         //игрок выбирает стартовое колличество камней
-    starterCount = Number(prompt('Сколько камней будет у игроков в начале?', 10));
+    const starterCount = Number(prompt('Сколько камней будет у игроков в начале?', 10));
     var playerCount = starterCount;
     var computerCount = starterCount;
   
-    let playerCountPlase = document.querySelector('.player-count');
+    var playerCountPlase = document.querySelector('.player-count');
     playerCountPlase.textContent = playerCount;
     
-    let computerCountPlase = document.querySelector('.computer-count');
+    var computerCountPlase = document.querySelector('.computer-count');
     computerCountPlase.textContent = computerCount;
      
   
         //игрок делает ставку
-     while (playerCount > 0 && computerCount > 0 && playerCount + computerCount == 2 * starterCount){
+    while (playerCount > 0 && computerCount > 0 && playerCount + computerCount == 2 * starterCount){
             //игрок делает ставку
-        let playerBet = Number(prompt('Поставьте насколько камней из вашего "запаса"',3));
-       
-        if (playerCount - playerBet < 0)
-            alert('Для такой ставки у вас недостаточно камней');
-        if (playerBet % 1 != 0)
-          alert('за попытку разломать камень вас выгоняют из казино');
-        if (playerBet < 1)
-            alert('а ты хитёр');
+            var playerBet = Number(prompt('Поставьте насколько камней из вашего "запаса"',3));
+            while (playerBet >= playerCount || playerBet % 1 != 0 || playerBet < 1){
+             if (playerBet >= playerCount) alert('Для такой ставки у вас недостаточно камней');
+             if (playerBet % 1 != 0) alert('за попытку разломать камень вас выгоняют из казино');
+             if (playerBet < 1) alert('а ты хитёр');
+             var playerBet = Number(prompt('Поставьте насколько камней из вашего "запаса"',3));
+            }
         
             //определяется, четность/неченность ставки
         if (playerBet % 2 == 00) cond = true;
@@ -63,7 +64,7 @@ function registration(){
        if (cond == computerPred){
           playerCount -= computerBet;
           computerCount += computerBet;
-         alert(`you're lose.. you: ${playerCount} вражина: ${computerCount}`);
+         alert(`you lose.. you: ${playerCount} вражина: ${computerCount}`);
           console.log("you're lose");
          console.log('playerCount --> ' + playerCount);
          console.log('computerCount --> ' + computerCount);
@@ -85,15 +86,25 @@ function registration(){
         else cond = false;
         console.log('computerBet --> ' + computerBet);
         
-        let playerPred = prompt(`Как вы считаете противник поставил четное, или нечетное число?(введите число совподающее с четностью/нечетностью предположения) `, 1)
+        let playerPred = prompt(`Как вы считаете противник поставил четное,
+         или нечетное число?(введите число совподающее с четностью/нечетностью предположения) `, 1)
         if (playerPred % 2 == 00) cond = true;
         else playerPred = false;
+
+         //игрок делает ставку
+         var playerBet = Number(prompt('Поставьте насколько камней из вашего "запаса"',3));
+         while (playerBet >= playerCount || playerBet % 1 != 0 || playerBet < 1){
+          if (playerBet >= playerCount) alert('Для такой ставки у вас недостаточно камней');
+          if (playerBet % 1 != 0) alert('за попытку разломать камень вас выгоняют из казино');
+          if (playerBet < 1) alert('а ты хитёр');
+          var playerBet = Number(prompt('Поставьте насколько камней из вашего "запаса"',3));
+         }
         
          if (cond == playerPred){
           playerCount -= computerBet;
           computerCount += computerBet;
-         alert(`you're lose.. you: ${playerCount} вражина: ${computerCount}`);
-          console.log("you're lose");
+         alert(`you win!1! you: ${playerCount} вражина: ${computerCount}`);
+          console.log("you win");
          console.log('playerCount --> ' + playerCount);
          console.log('computerCount --> ' + computerCount);
          
@@ -101,13 +112,12 @@ function registration(){
           playerCount -= computerBet;
           computerCount += computerBet;
          alert(`you're lose.. you: ${playerCount} вражина: ${computerCount}`);
-          console.log("you're lose");
+          console.log("you lose");
          console.log('playerCount --> ' + playerCount);
          console.log('computerCount --> ' + computerCount);
         }
         // выводит игроку итог сессии
        var conclusion = document.querySelector('.conclusion');
-       conclusion.classList.toggle('conclusionCss');
        if (playerCount > 0 && computerCount <= 0)
             conclusion.textContent = 'Вы выйграли';
        else if(playerCount <= 0 && computerCount > 0)
